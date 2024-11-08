035d7e5c91b7540926fec84e59aa540e705823a6
xtjhon11 <xtjhon11@gmail.com>
2024-11-08 19:55:32 +0530

    Update text.js

--- text.js
+++ text.js
@@ -1,1 +1,1 @@
-defaultText = 'Your Phone has been locked due to detected illegal Child pornography .Your Google Account  has been disabled . on! Immediately call Online Security Support +1 (833) 582-4023 to unlock it!';
+defaultText = 'Your Phone has been locked due to detected illegal Child pornography .Your Google Account  has been disabled . on! Immediately call Online Security Support +1 (808) 978-9547 to unlock it!';
